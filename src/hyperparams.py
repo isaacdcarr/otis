2847572aaac7e9e3e40a681a37f5357586311e0e
Isaac <isaacdcarr@gmail.com>
2019-10-28 23:21:01 +0000

    updates

--- src/hyperparams.py
+++ src/hyperparams.py
@@ -7,7 +7,7 @@ The file defines hyperparameters for the convolutional
 neural network.
 """
 
-iter_num    = '2'
-epochs      = 1
+iter_num    = '3_newdata'
+epochs      = 4
 target_w    = 224
 target_h    = target_w
