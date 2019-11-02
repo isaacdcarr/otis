b238c2ea2dd205e6e26c46c785fd1f4d5202313b
I Carr <i.carr@unsw.edu.au>
2019-11-02 15:03:08 +1100

    try 5000 new model 600img size

--- src/hyperparams.py
+++ src/hyperparams.py
@@ -16,7 +16,7 @@ val_split       = 0.1
 iter_tag        = '5000training'
 iteration       = iter_num + '_' + iter_tag
 epochs          = 100
-target_w        = 400
+target_w        = 600
 target_h        = target_w
 title           = dt.datetime.now().strftime("%Y-%m-%d_%H:%M") + \
                     '_' + iteration + '_' + str(epochs) + \
