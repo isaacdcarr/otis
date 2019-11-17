98cec96903ca0947ccdb6d46f2630cfe9cb0163c
Isaac <isaacdcarr@gmail.com>
2019-11-17 08:37:44 +0000

    final and try begin

--- src/hyperparams.py
+++ src/hyperparams.py
@@ -11,12 +11,12 @@ import datetime as dt
 
 save_model      = True
 single_input    = False
-iter_num        = '9'
+iter_num        = '11'
 val_split       = 0.1
-iter_tag        = 'all'
+iter_tag        = 'extralayer512'
 iteration       = iter_num + '_' + iter_tag
 epochs          = 250
-target_w        = 224
+target_w        = 300
 target_h        = target_w
 title           = dt.datetime.now().strftime("%Y-%m-%d_%H:%M") + \
                     '_' + iteration + '_' + str(epochs) + \
