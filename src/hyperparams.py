62694c377cf764bf633324caafc4c66504ea870a
I Carr <i.carr@unsw.edu.au>
2019-11-18 19:32:17 +1100

    try all data'

--- src/hyperparams.py
+++ src/hyperparams.py
@@ -11,12 +11,12 @@ import datetime as dt
 
 save_model      = True
 single_input    = False
-iter_num        = '14'
+iter_num        = '15'
 val_split       = 0.1
-iter_tag        = '1stdataset_testsingleoutput'
+iter_tag        = 'alldatasingleout_specificvalidationset'
 iteration       = iter_num + '_' + iter_tag
 epochs          = 250
-target_w        = 224
+target_w        = 300
 target_h        = target_w
 title           = dt.datetime.now().strftime("%Y-%m-%d_%H:%M") + \
                     '_' + iteration + '_' + str(epochs) + \
