7e6a9f49528d8051d5806c81727da6e0bf7c7c91
I Carr <i.carr@unsw.edu.au>
2019-11-19 22:48:10 +1100

    try all data new model

--- src/hyperparams.py
+++ src/hyperparams.py
@@ -16,7 +16,7 @@ val_split       = 0.1
 iter_tag        = 'newmodel_alldata'
 iteration       = iter_num + '_' + iter_tag
 epochs          = 100
-target_w        = 400
+target_w        = 300
 target_h        = target_w
 title           = dt.datetime.now().strftime("%Y-%m-%d_%H:%M") + \
                     '_' + iteration + '_' + str(epochs) + \
